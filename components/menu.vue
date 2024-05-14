a7b4f60eb62f8752cc455c300081c43dffe9e7d6
Your Name <your.email@example.com>
2024-05-14 11:38:05 -0700

    Added external prop to nuxt link in menu

--- components/menu.vue
+++ components/menu.vue
@@ -3,16 +3,16 @@
     <nav>
       <ul class="flex flex-col md:flex-row justify-center spaceu-y-1 md:space-y-0 md:gap-7 font-mono">
         <li>
-          <nuxt-link  class="link" to="/">Home</nuxt-link>
+          <nuxt-link  class="link" to="/" :external=true >Home</nuxt-link>
         </li>
         <li>
-          <nuxt-link  class="link" to="/about">About</nuxt-link>
+          <nuxt-link  class="link" to="/about" :external=true>About</nuxt-link>
         </li>
         <li>
-          <nuxt-link  class="link" to="/blog">Blog</nuxt-link>
+          <nuxt-link  class="link" to="/blog" :external=true>Blog</nuxt-link>
         </li>
         <li>
-          <nuxt-link class="link" to="/projects">Projects</nuxt-link>
+          <nuxt-link class="link" to="/projects" :external=true>Projects</nuxt-link>
         </li>
       </ul>
     </nav>
